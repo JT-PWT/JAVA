07a8dab6e1122378751cfd63c7b0fbe58a836ff2
Jeferson <jeferson.silva220@gmail.com>
2024-08-29 21:29:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Oi, Mundo");
+/*let preco = prompt("Doe para o Criança Esperança:");
+document.write(preco);*/
+
+let nome = prompt("Digite seu nome:");
+document.write("Olá, " , nome, ". Seja bem vindo!!!");
